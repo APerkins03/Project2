b41e5fa33cd9b23275a80f44c440e4c733d773e2
Amber Moody <aperkins0922@gmail.com>
2023-06-23 16:04:39 -0400

    update working successfully

--- routes/events.routes.js
+++ routes/events.routes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const Event = require('../models/events.model');
+const User = require('../models/user.model.js');
 
 // Render the eventchanges view and pass the event data
 router.get('/eventchanges', async (req, res) => {
