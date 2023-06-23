7018e8a157ec1a45fd04ca54760adfb7f611d555
Amber Moody <aperkins0922@gmail.com>
2023-06-23 17:27:49 -0400

    updated css

--- routes/events.routes.js
+++ routes/events.routes.js
@@ -91,12 +91,15 @@ router.put('/events/:id', async (req, res) => {
     if (!event) {
       return res.status(404).json({ error: 'Event not found' });
     }
+    res.redirect('/myprofile');
 
-    res.status(200).json({ message: 'Event updated successfully' });
+    // res.status(200).json({ message: 'Event updated successfully' });
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
 });
+
+
 
 // Delete an event by ID
 router.delete('/events/:id', async (req, res) => {
